da7842080f57e9a3dc164d31f94b6289006808a6
Gabriel <Gabrieldb050890@gmail.com>
2024-12-02 12:18:21 +0100

    commit creados archivos de conexion wifi con claves ocultas

--- poc/wifi/wifi.ino
+++ poc/wifi/wifi.ino
@@ -1,9 +1,15 @@
-void setup() {
-
-
+#include <WiFi.h>
+ 
+#include "config.h"  // Sustituir con datos de vuestra red
+#include "ESP32_C3_Utils.hpp"
+ 
+void setup()
+{
+  Serial.begin(9600);
+  
+  ConnectWiFi_STA();
 }
-
-void loop() {
-
-
+ 
+void loop() 
+{
 }
